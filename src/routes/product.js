a366541b29e07decac115710779aa156616f497d
Kamran Haider <kamran@bigfolio.co>
2024-03-14 04:36:35 +0500

    updated

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,35 +1,23 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const product = require("../controllers/product");
+const product = require('../controllers/product');
 
 // Import verifyToken function
-const verifyToken = require("../config/jwt");
+const verifyToken = require('../config/jwt');
 
 // admin routes
-router.post("/admin/product", verifyToken, product.createProduct);
-router.get("/admin/products", verifyToken, product.GetAllProducts);
-router.get(
-  "/admin/product/:id",
-  verifyToken,
-  product.getOneProductBySlug
-);
-router.put(
-  "/admin/product/:id",
-  verifyToken,
-  product.updateProductBySlug
-);
+router.post('/admin/product', verifyToken, product.createProduct);
+router.get('/admin/products', verifyToken, product.GetAllProducts);
+router.get('/admin/product/:id', verifyToken, product.getOneProductBySlug);
+router.put('/admin/product/:id', verifyToken, product.updateProductBySlug);
 
-router.delete(
-  "/admin/product/:id",
-  verifyToken,
-  product.deletedProductBySlug
-);
+router.delete('/admin/product/:id', verifyToken, product.deletedProductBySlug);
 
 // user routes
 // Define your routes here
-router.get("/products", product.GetAllProducts); // Add token verification middleware
-router.get("/filters/:category", product.getFiltersByCategory);
-router.get("/product/:slug", product.getOneProductBySlug);
-router.get("/filters", product.getAllFilters);
+router.get('/products', product.GetAllProducts); // Add token verification middleware
+router.get('/filters/:category', product.getFiltersByCategory);
+router.get('/product/:slug', product.getOneProductBySlug);
+router.get('/filters', product.getAllFilters);
 
 module.exports = router;
