ca7fc2cb12e8d3fc376ee2e6fb7a7b73218ba23d
Kamran Haider <151521656+kamran-bigfolio@users.noreply.github.com>
2024-03-23 02:48:04 +0500

    knk

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,14 +1,14 @@
-const express = require("express")
-const router = express.Router()
-const adminController = require("../controllers/admin")
-const verifyToken = require("../config/jwt")
+const express = require('express');
+const router = express.Router();
+const adminController = require('../controllers/admin');
+const verifyToken = require('../config/jwt');
 
-router.get("/admin/users", verifyToken, adminController.getUsersForAdmin)
+router.get('/admin/users', verifyToken, adminController.getUsersForAdmin);
 
-router.get("/admin/users/:id", verifyToken, adminController.getOrdersByUid)
+router.get('/admin/users/:id', verifyToken, adminController.getOrdersByUid);
 
-router.put("/admin/users/:id", verifyToken, adminController.UpdateRoleForAdmin)
+router.put('/admin/users/:id', verifyToken, adminController.UpdateRoleForAdmin);
 
 // eslint-disable-next-line no-undef
 
-module.exports = router
+module.exports = router;
