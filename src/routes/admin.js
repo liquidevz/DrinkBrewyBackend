c2323392d1bb210cbbf7ae455489295a7c63ca35
Hassan-Dawood <136306696+Hassan-Dawood@users.noreply.github.com>
2024-03-22 22:28:56 +0500

    update admin api

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,12 +1,16 @@
-const express = require("express");
-const router = express.Router();
-const adminController = require("../controllers/admin");
-const verifyToken = require("../config/jwt");
+const express = require("express")
+const router = express.Router()
+const adminController = require("../controllers/admin")
+const verifyToken = require("../config/jwt")
 
-router.get("/admin/users", adminController.getUsersForAdmin);
+router.get("/admin/users", verifyToken, adminController.getUsersForAdmin)
 
-router.get("/admin/users/:id", adminController.getOrdersByUid);
+router.get("/admin/users/:id", verifyToken, adminController.getOrdersByUid)
 
-router.put("/admin/users/:id", adminController.UpdateRoleForAdmin);
+router.put(
+	"/admin/users/role/:id",
+	verifyToken,
+	adminController.UpdateRoleForAdmin
+)
 
-module.exports = router;
+module.exports = router
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,20 +1,16 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/user');
+const express = require("express")
+const router = express.Router()
+const userController = require("../controllers/user")
 
-const verifyToken = require('../config/jwt');
-router.get('/users/profile', verifyToken, userController.getOneUser);
+const verifyToken = require("../config/jwt")
+router.get("/users/profile", verifyToken, userController.getOneUser)
 
-router.put('/users/profile', verifyToken, userController.updateUser);
+router.put("/users/profile", verifyToken, userController.updateUser)
 
-router.get('/users/invoice', verifyToken, userController.getInvoice);
+router.get("/users/invoice", verifyToken, userController.getInvoice)
 
-router.put(
-  '/users/change-password',
-  verifyToken,
-  userController.changePassword
-);
-router.post('/admin/users/:uid', verifyToken, userController.getUserByAdmin);
-router.post('/admin/users/role/:uid', verifyToken, userController.updateRole);
+router.put("/users/change-password", verifyToken, userController.changePassword)
+router.post("/admin/users/:uid", verifyToken, userController.getUserByAdmin)
+// router.post("/admin/users/role/:uid", verifyToken, userController.updateRole)
 
-module.exports = router;
+module.exports = router
