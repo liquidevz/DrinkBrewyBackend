dcc260e589e1c1fa45c05d1d86ccae15361abab6
Kamran Haider <151521656+kamran-bigfolio@users.noreply.github.com>
2024-03-26 23:55:41 +0500

    updated card and order

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-"use strict";
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const bodyParser = require("body-parser");
+'use strict';
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 // Load environment variables from .env file
 dotenv.config();
 
@@ -16,65 +16,64 @@ app.use(bodyParser.json());
 
 // Connect to MongoDB
 mongoose
-	.connect(
-		"mongodb+srv://vercel-admin-user:uKWAWdCpSoBDdApC@cluster0.whfrnxl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-		{
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		}
-	)
-	.then(() => {
-		console.log("Connected to MongoDB");
-	})
-	.catch(error => {
-		console.error("Error connecting to MongoDB:", error);
-	});
+  .connect(
+    'mongodb+srv://vercel-admin-user:uKWAWdCpSoBDdApC@cluster0.whfrnxl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 // Routes
-const homeRoutes = require("./routes/home");
-const authRoutes = require("./routes/auth");
-const brandRoutes = require("./routes/brand");
-const categoryRoutes = require("./routes/category");
-const subcategoryRoutes = require("./routes/subcategory");
-const newsletterRoutes = require("./routes/newsletter");
-const productRoutes = require("./routes/product");
-const dashboardRoutes = require("./routes/dashboard");
-const searchRoutes = require("./routes/search");
-const best_sellerRoutes = require("./routes/best-seller");
-const userRoutes = require("./routes/user");
-const cartRoutes = require("./routes/cart");
-const couponCodeRoutes = require("./routes/coupon-code");
-const reviewRoutes = require("./routes/review");
-const wishlistRoutes = require("./routes/wishlist");
-const OrderRoutes = require("./routes/order");
-const adminRoutes = require("./routes/admin");
-const paymentRoutes = require("./routes/payment-intents");
+const homeRoutes = require('./routes/home');
+const authRoutes = require('./routes/auth');
+const brandRoutes = require('./routes/brand');
+const categoryRoutes = require('./routes/category');
+const subcategoryRoutes = require('./routes/subcategory');
+const newsletterRoutes = require('./routes/newsletter');
+const productRoutes = require('./routes/product');
+const dashboardRoutes = require('./routes/dashboard');
+const searchRoutes = require('./routes/search');
 
-app.use("/api", homeRoutes);
-app.use("/api", authRoutes);
-app.use("/api", brandRoutes);
-app.use("/api", categoryRoutes);
-app.use("/api", subcategoryRoutes);
-app.use("/api", newsletterRoutes);
-app.use("/api", productRoutes);
-app.use("/api", dashboardRoutes);
-app.use("/api", searchRoutes);
-app.use("/api", best_sellerRoutes);
-app.use("/api", userRoutes);
-app.use("/api", cartRoutes);
-app.use("/api", couponCodeRoutes);
-app.use("/api", reviewRoutes);
-app.use("/api", wishlistRoutes);
-app.use("/api", OrderRoutes);
-app.use("/api", adminRoutes);
-app.use("/api", paymentRoutes);
+const userRoutes = require('./routes/user');
+const cartRoutes = require('./routes/cart');
+const couponCodeRoutes = require('./routes/coupon-code');
+const reviewRoutes = require('./routes/review');
+const wishlistRoutes = require('./routes/wishlist');
+const OrderRoutes = require('./routes/order');
+const adminRoutes = require('./routes/admin');
+const paymentRoutes = require('./routes/payment-intents');
+
+app.use('/api', homeRoutes);
+app.use('/api', authRoutes);
+app.use('/api', brandRoutes);
+app.use('/api', categoryRoutes);
+app.use('/api', subcategoryRoutes);
+app.use('/api', newsletterRoutes);
+app.use('/api', productRoutes);
+app.use('/api', dashboardRoutes);
+app.use('/api', searchRoutes);
+app.use('/api', userRoutes);
+app.use('/api', cartRoutes);
+app.use('/api', couponCodeRoutes);
+app.use('/api', reviewRoutes);
+app.use('/api', wishlistRoutes);
+app.use('/api', OrderRoutes);
+app.use('/api', adminRoutes);
+app.use('/api', paymentRoutes);
 
 // GET API
-app.get("/", (req, res) => {
-	res.send("This is a GET API");
+app.get('/', (req, res) => {
+  res.send('This is a GET API');
 });
 
 // Start the server
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
