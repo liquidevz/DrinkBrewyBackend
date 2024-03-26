3420709f96a8945f8eb66fc4b304ba2827a5dca9
Kamran Haider <151521656+kamran-bigfolio@users.noreply.github.com>
2024-03-26 22:14:10 +0500

    updated payment

--- src/controllers/payment-intents.js
+++ src/controllers/payment-intents.js
@@ -1,31 +1,24 @@
-const Stripe = require("stripe");
+const Stripe = require('stripe');
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 const currency = process.env.CURRENCY;
 
 const payment_intents = async (req, res) => {
-	if (req.method === "POST") {
-		try {
-			// Assuming req.body is already parsed
-			const { amount } = req.body;
+  try {
+    // Assuming req.body is already parsed
+    const { amount } = req.body;
 
-			const paymentIntent = await stripe.paymentIntents.create({
-				amount: amount * 100, // Convert to cents
-				currency: currency.toLowerCase(), // Convert currency to lowercase
-			});
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount * 100, // Convert to cents
+      currency: currency.toLowerCase(), // Convert currency to lowercase
+    });
 
-			return res
-				.status(200)
-				.json({ client_secret: paymentIntent.client_secret });
-		} catch (err) {
-			console.error(err); // Log the error for debugging
-			return res
-				.status(500)
-				.json({ success: false, message: "Internal server error" });
-		}
-	} else {
-		res.setHeader("Allow", "POST");
-		res.status(405).end("Method Not Allowed");
-	}
+    return res.status(200).json({ client_secret: paymentIntent.client_secret });
+  } catch (err) {
+    console.error(err); // Log the error for debugging
+    return res
+      .status(500)
+      .json({ success: false, message: 'Internal server error' });
+  }
 };
 
 module.exports = { payment_intents };
