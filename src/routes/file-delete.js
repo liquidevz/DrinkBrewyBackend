49b1fa026246a8e46d735ce85fc2da6f097812f6
Mohsin Dawood <mohsindawood58@gmail.com>
2024-03-28 01:45:57 +0500

    push add id in delete file

--- src/routes/file-delete.js
+++ src/routes/file-delete.js
@@ -1,6 +1,6 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const delete_fileController = require("../controllers/file-delete");
-router.delete("/delete-file", delete_fileController.deleteFile);
+const delete_fileController = require('../controllers/file-delete');
+router.delete('/delete-file/:id', delete_fileController.deleteFile);
 
 module.exports = router;
